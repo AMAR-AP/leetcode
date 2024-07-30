c50e030ab8881368ba2f7cc82ef1c6ba82af4671
AMAR-AP <114557114+AMAR-AP@users.noreply.github.com>
2024-07-30 21:53:42 +0530

    Add files via upload

---
+++ problem#118.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def generate(self, numRows):
+        if numRows<=1:
+            return [[1]]
+        nums=[[1]]
+        for i in range(1,numRows):
+            newnum=[1]
+            for j in range(len(nums[i-1])-1):
+                newnum.append(nums[i-1][j]+nums[i-1][j+1])
+            newnum.append(1)
+            nums.append(newnum)
+        return nums
