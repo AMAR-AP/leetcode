c666166f34e6601599efa8e47b9423266e116839
AMAR-AP <114557114+AMAR-AP@users.noreply.github.com>
2024-08-09 22:00:46 +0530

    Add files via upload

---
+++ problem#414.py
@@ -0,0 +1,12 @@
+class Solution:
+    def thirdMax(self, nums):
+        nums.sort(reverse=True)
+        c,n=1,len(nums)
+        m=nums[0]
+        for i in range(1,n):
+            if nums[i]<m:
+                c+=1
+                m=nums[i]
+            if c==3:
+                return m
+        return nums[0]
